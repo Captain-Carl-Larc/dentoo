6e679be8f1a6a1165858f35168f3c9dff8013959
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-30 18:50:40 +0300

    Imported benefits component

--- dentoo/src/App.jsx
+++ dentoo/src/App.jsx
@@ -2,12 +2,14 @@
 import "./App.css";
 import Hero from "./components/hero";
 import Features from "./components/features";
+import Benefits from "./components/benefits";
 
 function App() {
   return (
     <>
       <Hero />
       <Features />
+      <Benefits />
     </>
   );
 }
