966d411944f7db85054f0b3751ca56541428b979
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-30 17:12:20 +0300

    project set up -react & tailwind

---
+++ dentoo/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <span className="text-4xl text-blue-500">Dentoo</span>
+        <span className="text-2xl text-gray-700"> - A DentOS Experience</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
