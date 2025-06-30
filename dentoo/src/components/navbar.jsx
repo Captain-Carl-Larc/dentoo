3b299585235f8fd2744af503da30fcd83959845a
Stephen Kalwenzi <stephenmutwiiofficial@gmail.com>
2025-06-30 19:17:44 +0300

    typo fix

--- dentoo/src/components/navbar.jsx
+++ dentoo/src/components/navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
     <nav className="bg-white shadow-md fixed w-full z-10 top-0 left-0 p-4">
       <div className="logo">
         <h2 className="text-xl md:text-3xl font-bold text-blue-800">
-          SmartMama
+          Dentoo
         </h2>
       </div>
     </nav>
